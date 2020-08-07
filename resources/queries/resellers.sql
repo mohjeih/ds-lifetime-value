b404e522bb2ac4953a1224044aff274962336fea
Mohammad Jeihoonian <mohammad.jeihoonian@ssense.com>
2020-08-07 11:37:47 -0400

    Updated resellers

--- resources/queries/resellers.sql
+++ resources/queries/resellers.sql
@@ -2,9 +2,12 @@ select
 @colnames
 from (
 
-select
+select distinct
       memberID,
-      Flag_Reseller as flagReseller
-from  DS_RESELLER
+      max(confirmedFlag) as flagReseller
+from  do_hvc_cart_items
+where orderTime > DATE_SUB(CURDATE(), INTERVAL 104 WEEK)
+group by memberID
+having flagReseller = 1
 
 ) x
