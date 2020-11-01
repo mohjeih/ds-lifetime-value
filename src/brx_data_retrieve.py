dc2014d3e619dd67e64ff2cb0f85bf84a945f4e6
Mohammad Jeihoonian <mohammad.jeihoonian@ssense.com>
2020-11-01 12:43:00 -0500

    Updated environment.yml

--- src/brx_data_retrieve.py
+++ src/brx_data_retrieve.py
@@ -242,9 +242,10 @@ class BrxRet(DataRet):
 
         self.session_feat_ext(table_id='_session_features')
 
-        tables_to_delete = ['_products', '_employees', '_adwords', '_audiences', '_markdown', '_session_raw_agg',
-                            '_session_md', '_page_raw', '_page_features', '_pdp_features', '_session_features',
-                            '_users', '_app_session_raw_agg', '_app_session_md', '_app_page_raw']
+        tables_to_delete = ['_products']
+            # , '_employees', '_adwords', '_audiences', '_markdown', '_session_raw_agg',
+            #                 '_session_md', '_page_raw', '_page_features', '_pdp_features', '_session_features',
+            #                 '_users', '_app_session_raw_agg', '_app_session_md', '_app_page_raw']
 
         date_dataset = pd.DataFrame()
 
@@ -260,8 +261,9 @@ class BrxRet(DataRet):
 
             ads_dataset = self.sync(table_id='_ad_users_pt')
 
-            ext_tables_to_delete = tables_to_delete + ['_brx_features_pt', '_brx_sample', '_ad_users_pt',
-                                                       '_invoices']
+            ext_tables_to_delete = tables_to_delete
+                                   # + ['_brx_features_pt', '_brx_sample', '_ad_users_pt',
+                                   #                     '_invoices']
 
             delete_table(dataset_id=self.dataset_id, table_ids=ext_tables_to_delete)
 
